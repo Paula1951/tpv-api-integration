d4fa92c29bfc7e4f4f7131bef3935a499a40754f
Paula1951 <paulasr1951@gmail.com>
2025-03-17 00:20:26 +0100

    code refactoring

--- src/SalesFormat.php
+++ src/SalesFormat.php
@@ -1,16 +1,26 @@
 <?php
 
 use src\AgoraAPI;
+use Dotenv\Dotenv;
 
 class SalesFormat 
 {
+    public string $endpointUrl;
+
+    public function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        $ticketDay = getenv('TICKET_DAY');
+        $enpointTicketDay = getenv('ENDPOINT_TICKET_DAY');
+
+        $this->endpointUrl = $enpointTicketDay . urlencode($ticketDay);
+    }
+
     public function getTicketsDay()
     {
-        $ticketDay = '2023-10-01';
-        $enpoint = "api/tickets?date=" . urlencode($ticketDay);
-
         $agoraAPI = new AgoraAPI();
-        $apiResponse = $agoraAPI->connectionApi($enpoint);
+        $apiResponse = $agoraAPI->connectionApi($this->endpointUrl);
         if ($apiResponse === false) {
             return ; 
         }
@@ -22,7 +32,6 @@ class SalesFormat
     {
         $tickets = $this->getTicketsDay();
         $lines = $tickets['Tickets'][0]['Lines'];
-
         return $lines;
     }
 
@@ -32,36 +41,52 @@ class SalesFormat
         }));
     }
 
+    public function buildLineData($line, $discountType, $discountAmount, $discountConcept, $taxRate)
+    {
+        return [
+            'product_id' => $line['ProductId'],
+            'name' => $line['ProductName'],
+            'price' => $line['ProductPrice'],
+            'quantity' => $line['Quantity'],
+            'discount_type' => $discountType,
+            'discount_amount' => $discountAmount,
+            'discount_concept' => $discountConcept,
+            'tax_rate' => $taxRate,
+            'modifiers' => []
+        ];
+    }
+
+    public function addModifiersToLine($lineData, $addins) {
+        foreach ($addins as $addin) {
+            $lineData['modifiers'][] = [
+                'modifier_id' => $addin['ProductId'],
+                'name' => $addin['ProductName'],
+                'price' => $addin['ProductPrice'],
+                'quantity' => 1
+            ];
+        }
+        return $lineData;
+    }
+
+
     public function fillLines($linesData) {
         $lines = [];
 
         foreach ($linesData as $line) {
 
-            if ($line['DiscountRate'] == $line['CashDiscount']) {
-                $discountType = 'N/A';
-            } else {
-                $discountType = ($line['DiscountRate'] > 0) ? 'percentage' : 'amount';
-            }            
-
+            $discountType = $this->determineDiscountType($line);
             $discountAmount = ($discountType === 'percentage') ? $line['DiscountRate'] * 100 : $line['CashDiscount'];
+            $discountConcept = $this->getDiscountConcept($line);
 
-            $discountConcept = 'N/A';
-            if (!empty($line['Offers'])) {
-                $discountConcept = $line['Offers'][0]['ApplicationMode'];
-            }
             $taxRate = $line['VatRate'] * 100;
              
-            $lineData = [
-                'product_id' => $line['ProductId'],
-                'name' => $line['ProductName'],
-                'price' => $line['ProductPrice'],
-                'quantity' => $line['Quantity'],
-                'discount_type' => $discountType,
-                'discount_amount' => $discountAmount,
-                'discount_concept' => $discountConcept,
-                'tax_rate' => $taxRate,
-                'modifiers' => []
-            ];
+            $lineData = $this->buildLineData(
+                $line, 
+                $discountType, 
+                $discountAmount, 
+                $discountConcept, 
+                $taxRate
+            );
 
             foreach ($line['Addins'] as $addin) {
                 $modifierData = [
@@ -82,46 +107,74 @@ class SalesFormat
         return $lines;
     }
 
+    public function formatCloseTime($date)
+    {
+        return date('Y-m-d H:i:s', strtotime($date));
+    }
+    
+    public function determineDiscountType($discount)
+    {
+        if ($discount['DiscountRate'] == $discount['CashDiscount']) {
+            return 'N/A';
+        }
+
+        return ($discount['DiscountRate'] > 0) ? 'percentage' : 'amount';
+    }
+
+    public function getDiscountConcept($jsonTicketData)
+    {
+        if (!empty($jsonTicketData['Offers'])) {
+            return $jsonTicketData['Offers'][0]['ApplicationMode'];
+        }
+
+        return 'N/A';
+    }
+
+    public function buildJsonData($id, $close_time, $total, $discountType, $discountAmount, $discountConcept)
+    {
+        return [
+            "id" => $id,
+            "close_time" => $close_time,
+            "total" => $total,
+            "discount_type" => $discountType,
+            "discount_amount" => $discountAmount,
+            "discount_concept" => $discountConcept,
+            "lines" => []
+        ];
+    }
+
     public function formatSale()
     {
         $ticketData = $this->getTicketsDay();
 
         if (is_string($ticketData)) {
-            $id = uniqid();
-
-            $close_time = date('Y-m-d H:i:s', strtotime($ticketData['Date']));
-
             $jsonTicketData = json_decode($ticketData);
+
+            $id = uniqid();
+            $close_time = $this->formatCloseTime($jsonTicketData['Date']);
             $total = $jsonTicketData['Totals']['GrossAmount'];
 
             $discount = $jsonTicketData['Discounts'];
-
-            if ($discount['DiscountRate'] == $discount['CashDiscount']) {
-                $discountType = 'N/A';
-            } else {
-                $discountType = ($discount['DiscountRate'] > 0) ? 'percentage' : 'amount';
-            }
-
+            $discountType = $this->determineDiscountType($discount);
             $discountAmount = ($discountType === 'percentage') ? $discount['DiscountRate'] * 100 : $discount['CashDiscount'];
-
-            $discountConcept = 'N/A';
-            if (!empty($jsonTicketData['Offers'])) {
-                $discountConcept = $jsonTicketData['Offers'][0]['ApplicationMode'];
-            }
+            $discountConcept = $this->getDiscountConcept($jsonTicketData);
         
-            $jsonData = [
-                "id" => $id,
-                "close_time" => $close_time,
-                "total" => $total,
-                "discount_type" => $discountType,
-                "discount_amount" => $discountAmount,
-                "discount_concept" => $discountConcept,
-                "lines" => []
-            ];
+            $jsonData = $this->buildJsonData(
+                $id, 
+                $close_time, 
+                $total, 
+                $discountType,
+                $discountAmount, 
+                $discountConcept
+            );
 
             $linesData = $this->getLinesTickets();
             $filledLines = $this->fillLines($linesData);
             $jsonData["lines"] = $filledLines;
+
+            return $jsonData;
         }
+
+        return null;
     }
 }
